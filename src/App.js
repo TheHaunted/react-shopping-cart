0a9518ada5e1c5ffb639294b88a5141065ccdf2b
Eric Cabrera <Ecabrera@Erics-MacBook-Pro.local>
2019-08-12 17:46:48 -0500

    .

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 					<Route exact path="/" component={Products} />
 
 					<Route path="/cart" component={ShoppingCart} />
-					</CartContext.Provider>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
